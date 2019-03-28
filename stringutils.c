73d8415ea31ff0fae7057705dc4a1327449017a2
Edward <zhengzhijin@aliyun.com>
2019-03-28 16:23:58 +0800

    Add readAllLinesW

--- stringutils.c
+++ stringutils.c
@@ -4,6 +4,69 @@
 
 #include "stringutils.h"
 #include <stdlib.h>
+#include "linkedarray.h"
+
+int readAllLinesW(FILE *fileDesc, wchar_t ***lines,wchar_t **buffer) {
+    int lengthOfFile = 0;
+    unsigned char *bytesOfFile;
+    int lengthOfWC = 0;
+    wchar_t *codepointsFromFile;
+
+    bytesOfFile = readAllByteFromFile(fileDesc, &lengthOfFile);
+    if (!bytesOfFile)return EOF;
+    bytesOfFile = realloc(bytesOfFile, lengthOfFile + 1);
+    bytesOfFile[lengthOfFile] = '\0';
+    lengthOfWC = mbstowcs(NULL, bytesOfFile, lengthOfFile + 1);
+    codepointsFromFile = (wchar_t *) malloc(sizeof(wchar_t) * (lengthOfWC + 1));
+    mbstowcs(codepointsFromFile, bytesOfFile, lengthOfFile + 1);
+    free(bytesOfFile);
+    *buffer=codepointsFromFile;
+    {
+        Node *ptrHead = 0;
+        int index = 0;
+
+        while (codepointsFromFile[index] != 0) {
+            //escape CRLF;
+            while (codepointsFromFile[index] == '\r' || codepointsFromFile[index] == '\n') {
+                index++;
+            }
+
+            if (codepointsFromFile[index] == 0) {
+                break;
+            }
+
+            linkArrayAppend(&ptrHead, codepointsFromFile + index);
+            while (codepointsFromFile[index] != '\r' || codepointsFromFile[index] != '\n' ||
+                   codepointsFromFile[index] != 0) {
+                index++;
+            }
+            if (codepointsFromFile[index] == 0) {
+                break;
+            }
+            codepointsFromFile[index] = 0;
+            index++;
+        }
+        if(ptrHead==0){
+            *buffer=0;
+            *lines=0;
+            return 0;
+        }else{
+            int countOfValidLine=linkArraryLength(ptrHead);
+            Node *temp_ptrHead=ptrHead;
+            int index=0;
+            *lines=(wchar_t **)malloc(sizeof(wchar_t *)*countOfValidLine);
+            while (temp_ptrHead!=NULL){
+                (*lines)[index]=(wchar_t *)temp_ptrHead->data;
+                temp_ptrHead=temp_ptrHead->next;
+                index++;
+            }
+            linkArraryDestroy(ptrHead);
+            return 1;
+        }
+    }
+
+}
+
 
 int readNextString(FILE *fileDesc, char **ptr) {
     char bufferSize = 64;
@@ -37,7 +100,7 @@ int readNextString(FILE *fileDesc, char **ptr) {
             *ptr = buffer;
             if (data == EOF) {
                 return EOF;
-            } else{
+            } else {
                 return 0;
             }
         }
@@ -47,21 +110,24 @@ int readNextString(FILE *fileDesc, char **ptr) {
     } while (1);
 
 }
-void printBinary(char ch){
-    unsigned  char p=0x80;
-    while(p){
-        p&ch?putchar('1'):putchar('0');
-        p>>=1;
+
+void printBinary(char ch) {
+    unsigned char p = 0x80;
+    while (p) {
+        p & ch ? putchar('1') : putchar('0');
+        p >>= 1;
     }
 }
-void printStringThroughBinaryForm(char *p,int size){
-    for(int i=0;i<size;i++){
-        printf("[%d]",i+1);
+
+void printStringThroughBinaryForm(char *p, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("[%d]", i + 1);
         printBinary(p[i]);
         putchar(' ');
     }
     putchar('\n');
 }
+
 char *readAllByteFromFile(FILE *file, int *length) {
     int bufferSize = 64 * 1024;
     int bufferSizeIncrese = 64 * 1024;
